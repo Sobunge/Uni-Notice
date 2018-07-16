96656689185814d99307b3d4f41fc863e9211ce4
Samuel Obunge <Samuelobunge@gmail.com>
2018-07-16 14:24:28 +0300

    Making changes to the community model, controller and view

--- UniNotice/src/main/java/com/example/uninotice/controller/CommunityController.java
+++ UniNotice/src/main/java/com/example/uninotice/controller/CommunityController.java
@@ -1,10 +1,13 @@
 package com.example.uninotice.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.uninotice.model.community.Community;
 import com.example.uninotice.security.community.CommunityService;
 
 @Controller
@@ -16,7 +19,13 @@ public class CommunityController {
 	@RequestMapping("community")
 	public String community(Model model) {
 		
+		ArrayList<Community> communities  = new ArrayList<>();
+		
+		communities.addAll(communityService.getAllCommunities());
+		
+		model.addAttribute("communities", communities);
 		model.addAttribute("title", "Uni-Notice");
+	
 		
 		return "community";
 	}
